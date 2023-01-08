65b65539c534ee25109ebb8362f9fd9d81fb4b4a
Che <43485962+c-git@users.noreply.github.com>
2023-01-08 18:20:54 -0500

    Copy over from js_lib

---
+++ lib.js
@@ -0,0 +1,118 @@
+// Version 1.19 (NOTE: This library has been DEPRECATED. SUPERSEDED by ts_lib)
+
+/**
+ * Function to simplify checking if object is defined
+ *
+ * @param {Object} obj object to be checked
+ * @returns {Boolean} true if object is passed is undefined
+ */
+function isUndefined(obj) {
+    return typeof obj === "undefined";
+}
+
+/**
+ * Use to create a run once version of a function
+ * Example usage:
+ *
+ * let myRunOnceFunc = once(function (value) {
+ *   console.log('Fired!');
+ *   return "Input value was " + value;
+ * });
+ *
+ * console.log(myRunOnceFunc(123)); //"Fired" and "Input value was 123"
+ * console.log(myRunOnceFunc(2)); //Only "Input value was 123"
+ *
+ * @param {function} fn function to be converted
+ * @param {type} context not sure, more testing needed
+ * @returns {function} The run only once version of fn
+ */
+function once(fn, context) {
+    let result;
+
+    return function () {
+        if (fn) {
+            result = fn.apply(context || this, arguments);
+            fn = null;
+        }
+
+        return result;
+    };
+}
+
+/**
+ * Asserts the condition and if it fails throws an exception.
+ *
+ * Patterned on <https://gist.github.com/jeromeetienne/2651899>
+ *
+ * @param cond The condition that should be true
+ * @param text The text to show on assert failure
+ */
+function assertOrDie(cond, text) {
+    if (!cond)
+        throw new Error(text || "Assertion failed!");
+}
+
+/**
+ * Only considers the time and checks if `x` is greater than `y`
+ * @param a {Date} the first time to check
+ * @param b {Date} the second time to check
+ * @returns {boolean} true if a > b, considering only time and false otherwise
+ */
+function isTimeGreater(a, b) {
+    return a.getHours() > b.getHours() ||
+        (a.getHours() === b.getHours() && a.getMinutes() > b.getMinutes());
+}
+
+/**
+ * Checks if two date/time values are on the same day (ignoring the time)
+ * @param {Date} a the first date to check
+ * @param {Date} b the second date to check
+ * @returns {boolean} true if they are on the same day and false otherwise
+ */
+function isSameDay(a, b) {
+    return a.getDate() === b.getDate()
+        && a.getMonth() === b.getMonth()
+        && a.getFullYear() === b.getFullYear();
+}
+
+/**
+ * Returns only the date portion of the date passed
+ * @param aDate The datetime to extract the date from
+ * @returns {Date} Date portion of date passed in
+ */
+function getDateOnly(aDate) {
+    return new Date(aDate.getFullYear(), aDate.getMonth(), aDate.getDate());
+}
+
+/**
+ * Prepends 0's if number has less than `minDigits` before the decimal point
+ * @param num {number} - The number to prepend the 0's to
+ * @param minDigits {number} The minimum number of digits before the decimal point
+ * @returns {string} The number prefixed with 0's as needed
+ */
+function zeroPad(num, minDigits) {
+    const intPart = Math.floor(num);
+    const digitsNeeded = minDigits - (intPart + '').length;
+    let prefix = '';
+    while (prefix.length < digitsNeeded) {
+        prefix += '0';
+    }
+    return prefix + num;
+}
+
+/**
+ * Check if the date/time value passed is on the Google Sheets Epoch (independent of the time)
+ * @param {Date} d the date to check
+ * @returns {boolean} true if it is on the Epoch and false otherwise
+ */
+function isOnEpochGSheets(d) {
+    return isSameDay(d, getEpochGSheets());
+}
+
+/**
+ * Returns the Epoch used by Google Sheets
+ * @returns {Date} Google Sheets Epoch
+ */
+function getEpochGSheets() {
+    return new Date(1899, 11, 30);
+}
---
+++ lib_gscript.js
@@ -0,0 +1,51 @@
+/*
+File to store google script specific library functions
+ */
+
+/**
+ * @OnlyCurrentDoc
+ */
+
+
+function alertInfo(aMsg) {
+    Logger.log(aMsg);
+    SpreadsheetApp.getActiveSpreadsheet().toast(aMsg);
+}
+
+function alertError(aMsg) {
+    aMsg = '⚠️' + ' Error: ' + aMsg;
+    Logger.log(aMsg);
+    SpreadsheetApp.getUi().alert(aMsg);
+}
+
+/**
+ * Gets a named range given its name
+ * @param rangeName {string} - The name of the named range
+ * @param spreadsheet - The spreadsheet to use, or active if not defined
+ * @returns {null|NamedRange} The named range if found else null
+ */
+function getNamedRange(rangeName, spreadsheet = undefined) {
+    if (isUndefined(spreadsheet)) {
+        spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    }
+
+    const namedRanges = spreadsheet.getNamedRanges();
+    for (let i = 0; i < namedRanges.length; i++) {
+        if (namedRanges[i].getName() === rangeName) {
+            return namedRanges[i];
+        }
+    }
+    return null; /// Range not found
+}
+
+/**
+ * Prompts the user to confirm a destructive action and returns true if the user decided to continue
+ * @param title The title of the dialog shown
+ * @param prompt The message to display to the user
+ */
+function confirmDestructiveAction(title, prompt) {
+    const ui = SpreadsheetApp.getUi();
+    const response = ui.alert(title, '⚠️' + ' Warning: ' + prompt, ui.ButtonSet.OK_CANCEL);
+
+    return response === ui.Button.OK;
+}
