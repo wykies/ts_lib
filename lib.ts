18c911a76a603367da9e40b5dd9a337372d9306d
Che <43485962+c-git@users.noreply.github.com>
2023-01-08 19:36:51 -0500

    Add types to library

--- lib.ts
+++ lib.ts
@@ -1,4 +1,5 @@
 // Version 0.1
+// noinspection JSUnusedGlobalSymbols
 
 /**
  * Use to create a run once version of a function
@@ -12,11 +13,11 @@
  * console.log(myRunOnceFunc(123)); //"Fired" and "Input value was 123"
  * console.log(myRunOnceFunc(2)); //Only "Input value was 123"
  *
- * @param {function} fn function to be converted
- * @param {type} context not sure, more testing needed
- * @returns {function} The run only once version of fn
+ * @param fn function to be converted
+ * @param context not sure, more testing needed
+ * @returns The run only once version of fn
  */
-export function once(fn, context) {
+export function once(fn: Function, context: any): Function {
     let result;
 
     return function () {
@@ -37,29 +38,29 @@ export function once(fn, context) {
  * @param cond The condition that should be true
  * @param text The text to show on assert failure
  */
-export function assertOrDie(cond, text) {
+export function assertOrDie(cond: boolean, text: string) {
     if (!cond)
         throw new Error(text || "Assertion failed!");
 }
 
 /**
  * Only considers the time and checks if `x` is greater than `y`
- * @param a {Date} the first time to check
- * @param b {Date} the second time to check
- * @returns {boolean} true if a > b, considering only time and false otherwise
+ * @param a the first time to check
+ * @param b the second time to check
+ * @returns true if a > b, considering only time and false otherwise
  */
-export function isTimeGreater(a, b) {
+export function isTimeGreater(a: Date, b: Date): boolean {
     return a.getHours() > b.getHours() ||
         (a.getHours() === b.getHours() && a.getMinutes() > b.getMinutes());
 }
 
 /**
  * Checks if two date/time values are on the same day (ignoring the time)
- * @param {Date} a the first date to check
- * @param {Date} b the second date to check
- * @returns {boolean} true if they are on the same day and false otherwise
+ * @param a the first date to check
+ * @param b the second date to check
+ * @returns true if they are on the same day and false otherwise
  */
-export function isSameDay(a, b) {
+export function isSameDay(a: Date, b: Date): boolean {
     return a.getDate() === b.getDate()
         && a.getMonth() === b.getMonth()
         && a.getFullYear() === b.getFullYear();
@@ -68,19 +69,19 @@ export function isSameDay(a, b) {
 /**
  * Returns only the date portion of the date passed
  * @param aDate The datetime to extract the date from
- * @returns {Date} Date portion of date passed in
+ * @returns Date portion of date passed in
  */
-export function getDateOnly(aDate) {
+export function getDateOnly(aDate: Date): Date {
     return new Date(aDate.getFullYear(), aDate.getMonth(), aDate.getDate());
 }
 
 /**
  * Prepends 0's if number has less than `minDigits` before the decimal point
- * @param num {number} - The number to prepend the 0's to
- * @param minDigits {number} The minimum number of digits before the decimal point
- * @returns {string} The number prefixed with 0's as needed
+ * @param num The number to prepend the 0's to
+ * @param minDigits The minimum number of digits before the decimal point
+ * @returns The number prefixed with 0's as needed
  */
-export function zeroPad(num, minDigits) {
+export function zeroPad(num: number, minDigits: number): string {
     const intPart = Math.floor(num);
     const digitsNeeded = minDigits - (intPart + '').length;
     let prefix = '';
@@ -92,17 +93,17 @@ export function zeroPad(num, minDigits) {
 
 /**
  * Check if the date/time value passed is on the Google Sheets Epoch (independent of the time)
- * @param {Date} d the date to check
- * @returns {boolean} true if it is on the Epoch and false otherwise
+ * @param d the date to check
+ * @returns true if it is on the Epoch and false otherwise
  */
-export function isOnEpochGSheets(d) {
+export function isOnEpochGSheets(d: Date): boolean {
     return isSameDay(d, getEpochGSheets());
 }
 
 /**
  * Returns the Epoch used by Google Sheets
- * @returns {Date} Google Sheets Epoch
+ * @returns Google Sheets Epoch
  */
-export function getEpochGSheets() {
+export function getEpochGSheets(): Date {
     return new Date(1899, 11, 30);
 }
--- lib_gscript.ts
+++ lib_gscript.ts
@@ -1,20 +1,24 @@
+// noinspection JSUnusedGlobalSymbols
+
 /*
 File to store google script specific library functions
  */
 
-import {isUndefined} from "./lib";
+
+import Spreadsheet = GoogleAppsScript.Spreadsheet.Spreadsheet;
+import NamedRange = GoogleAppsScript.Spreadsheet.NamedRange;
 
 /**
  * @OnlyCurrentDoc
  */
 
 
-export function alertInfo(aMsg) {
+export function alertInfo(aMsg: string) {
     Logger.log(aMsg);
     SpreadsheetApp.getActiveSpreadsheet().toast(aMsg);
 }
 
-export function alertError(aMsg) {
+export function alertError(aMsg: string) {
     aMsg = '⚠️' + ' Error: ' + aMsg;
     Logger.log(aMsg);
     SpreadsheetApp.getUi().alert(aMsg);
@@ -22,12 +26,12 @@ export function alertError(aMsg) {
 
 /**
  * Gets a named range given its name
- * @param rangeName {string} - The name of the named range
- * @param spreadsheet - The spreadsheet to use, or active if not defined
- * @returns {null|NamedRange} The named range if found else null
+ * @param rangeName The name of the named range
+ * @param spreadsheet The spreadsheet to use, or active if not defined
+ * @returns The named range if found else null
  */
-export function getNamedRange(rangeName, spreadsheet = undefined) {
-    if (isUndefined(spreadsheet)) {
+export function getNamedRange(rangeName: string, spreadsheet: Spreadsheet = undefined): NamedRange {
+    if (spreadsheet === undefined) {
         spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
     }
 
@@ -44,8 +48,9 @@ export function getNamedRange(rangeName, spreadsheet = undefined) {
  * Prompts the user to confirm a destructive action and returns true if the user decided to continue
  * @param title The title of the dialog shown
  * @param prompt The message to display to the user
+ * @returns true if OK was clicked else false
  */
-export function confirmDestructiveAction(title, prompt) {
+export function confirmDestructiveAction(title: string, prompt: string): boolean {
     const ui = SpreadsheetApp.getUi();
     const response = ui.alert(title, '⚠️' + ' Warning: ' + prompt, ui.ButtonSet.OK_CANCEL);
 
